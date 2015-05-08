1fb7094e528cb81ad6c3d99e7e71105e225aa3a8
mikasa <mikasanow@gmail.com>
2015-05-09 01:45:20 +0900

    update
    整理、ParticleLine追加

---
+++ Particles/Assets/Particles/Scripts/MoveByMouse.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveByMouse : MonoBehaviour
+{
+    void Update()
+    {
+        var mousePos = Input.mousePosition;
+        mousePos.z = 10f;
+        transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+    }
+}
